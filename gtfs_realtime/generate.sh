431619e0af33d191960f66152176b8feda1f17cf
Sam Cohen <samc1213@gmail.com>
2023-05-30 12:57:48 -0600

    Install protoc-gen-go

--- gtfs_realtime/generate.sh
+++ gtfs_realtime/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #//go:generate -comand generate.sh
 folder="$(dirname "$(readlink -f "$0")")"
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 protoc \
     --go_opt=Mgtfs-realtime.proto=./gtfs_realtime \
     --go_opt=paths=source_relative \
